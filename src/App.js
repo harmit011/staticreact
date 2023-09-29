16da923ac0c0cf95f102c386d8e0c6659a107a0c
Harmit Narula <45949440+harmit011@users.noreply.github.com>
2023-09-29 14:10:05 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const value = 'CSCI5902 F23 Students';
+  return <div>
+    <h1>Hello {value}</h1>
+    <h2>This is my static app</h2>
+    <p>I can add some random text here and update my application</p>
+    </div>;
 }
 
 export default App;
